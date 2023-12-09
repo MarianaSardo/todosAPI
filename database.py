07ba997f9607f3da66f4e981858417d30ad16bea
MarianaSardo <linkerita@gmail.com>
2023-12-09 13:34:37 -0300

    db en elephantsql

--- database.py
+++ database.py
@@ -1,15 +1,22 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-import os
+'''import os
+
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 
+'''
+
+SQLALCHEMY_DATABASE_URL = "postgresql://tvbbeiof:Q874-EKT6qC7HWqXZb0GbC862OBIvX76@motty.db.elephantsql.com/tvbbeiof"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
